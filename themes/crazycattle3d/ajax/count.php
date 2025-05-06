bab3fa6215f2f1b0c20b30971245028e20fb1db8
watermelonsqueaks <135186915+watermelonsqueaks@users.noreply.github.com>
2025-05-05 20:55:44 -0700

    Delete themes directory

--- themes/crazycattle3d/ajax/count.php
+++
@@ -1,4 +0,0 @@
-<?php
-$id = load_request()->get_value('id');
-$ret  = \helper\game::update_views($id);
-echo $ret;
--- themes/crazycattle3d/ajax/like-game.php
+++
@@ -1,5 +0,0 @@
-<?php
-$id_game = $_POST['id_game'];
-if ($id_game) {
-    $id_save = \helper\game::increate_like($id_game);
-}
